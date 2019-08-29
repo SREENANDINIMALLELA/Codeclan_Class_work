42427aa55d007488ebf04ee49e9c7605844af485
sreenandini <mallelasreenandini2@gmail.com>
2019-08-29 11:35:16 +0100

    my first d3 classpro

---
+++ week_01/day_4/specs/tests.rb
@@ -0,0 +1,25 @@
+require ('minitest/autorun')
+require ('minitest/rg')
+require_relative('../fizzBuzz.rb')
+
+class TestFile < MiniTest::Test
+  def test_fizz_buzz_3_returns_fizz
+    assert_equal("Fizz", fizz_buzz(3))
+  end
+  def test_fizz_buzz_5_returns_fizz
+    assert_equal("Buzz", fizz_buzz(5))
+  end
+  def test_fizz_buzz_15_returns_fizz
+    expected = "FizzBuzz"
+    actual = fizz_buzz(15)
+    assert_equal(expected ,actual  )
+  end
+  def test_fizz_buzz_2_returns_2_as_string
+   assert_equal("2", fizz_buzz(2))
+  end
+  def test_fizz_buzz_2_returns_2_as_string
+   assert_equal("2", fizz_buzz(2))
+  end
+
+
+    end
